73c366f4fc5ad3915c2eb211db987714f03f2035
danielsellergren <danielsellergren@gmail.com>
2023-03-19 14:41:06 -0400

    Adds initial public profile page for Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  # Root for Welcome/Marketing page.
   root "pages#home"
+
+  # Devise for authentication, includes OmniauthController for callbacks.
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth' }
+
+  # Public route for Users.
+  resources :users, only: [:show]
 end
